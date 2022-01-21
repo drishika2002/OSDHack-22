356761e8900cd33355838fde4f8371c09bdde181
Himanshi baliyan <76565275+HimanshiBaliyan@users.noreply.github.com>
2022-01-21 23:21:33 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_POST['submit']) )
+{
+
+     $con = mysqli_connect("localhost","root","");
+     mysqli_select_db($con, "meme");
+     if(mysqli_error($con)){
+         die("connection to this database failed due to".mysqli_connect_error());
+     }
+
+     
+     $s="select * from userregistration where email='$_POST[email]' && password='$_POST[password]'";
+     $result= mysqli_query($con,$s);
+     $num = mysqli_num_rows($result);
+
+if( $num ==1){
+    //
+}
+else{
+	header('location:index.php');
+}
+
+     mysqli_close($con);
+}
+?>
