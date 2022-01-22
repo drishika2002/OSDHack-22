51d36322489eac4db2f69860fef2b662589ae3a2
Himanshi baliyan <76565275+HimanshiBaliyan@users.noreply.github.com>
2022-01-22 16:28:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,25 @@
 <?php
 if (isset($_POST['submit']) )
-    {
-        $con = mysqli_connect("localhost","root","");
-        mysqli_select_db($con, "meme");
+{
 
-        if(mysqli_error($con)) {
-            die("connection to this database failed due to".mysqli_connect_error());
-        }
+     $con = mysqli_connect("localhost","root","");
+     mysqli_select_db($con, "meme");
+     if(mysqli_error($con)){
+         die("connection to this database failed due to".mysqli_connect_error());
+     }
 
-        $password = $_POST['password'];
-        $password2 = $_POST['password2'];
-        $s="select * from userregistration where username='$_POST[username]'";
-        $result= mysqli_query($con,$s);
-        $num = mysqli_num_rows($result);
+     
+     $s="select * from userregistration where email='$_POST[email]' && password='$_POST[password]'";
+     $result= mysqli_query($con,$s);
+     $num = mysqli_num_rows($result);
 
-        if($num == 1) {
-            echo "Username already taken";
-        } elseif($password != $password2) {
-            echo "<script>alert('Password do not match!'); window.location='signup.php'</script>";
-        } else {
-            $sql="INSERT INTO userregistration ( email, username, number,password) VALUES ( '$_POST[email]', '$_POST[username]', '$_POST[number]', '$_POST[password]')";
-            mysqli_query($con, $sql);
-            echo"Registration successful";
-        }
+if( $num ==1){
+    header('location:home.html');
+}
+else{
+	header('location:index.php');
+}
 
-        mysqli_close($con);
+     mysqli_close($con);
 }
 ?>
